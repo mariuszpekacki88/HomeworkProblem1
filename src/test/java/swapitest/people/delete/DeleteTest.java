c96f26498c5d0e34b6d6520c99bee2dba5f96c39
Mariusz Pękacki <mariusz.pekacki@luxmed.pl>
2022-08-10 12:35:58 +0200

    addreszta

---
+++ src/test/java/swapitest/people/delete/DeleteTest.java
@@ -0,0 +1,19 @@
+package swapitest.people.delete;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import swapitest.base.BaseTest;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteTest extends BaseTest {
+    @Test
+    public void detetePeople() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .delete(BASE_URL + "/" + PEOPLE + "/1")
+                .then()
+                .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+    }
+}
